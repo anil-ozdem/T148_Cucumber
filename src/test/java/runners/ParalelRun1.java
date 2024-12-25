0538996ebbeef8b6e275c10eb002e718eb3c536e
anilozdem <anil.ozdem2134@gmail.com>
2024-12-25 13:45:19 +0300

    Ders20 Sonu

--- src/test/java/runners/ParalelRun1.java
+++ src/test/java/runners/ParalelRun1.java
@@ -9,14 +9,15 @@ import org.junit.platform.suite.api.Suite;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("src/test/java")
-@ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME, value = "src/test/resources/features")
-@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "stepdefinitions")
-@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@paralel1")
-@ConfigurationParameter(key = Constants.EXECUTION_DRY_RUN_PROPERTY_NAME, value = "false")
+@ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME,value = "src/test/resources/features")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME,value = "stepdefinitions")
+@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME,value = "@paralel1")
+@ConfigurationParameter(key = Constants.EXECUTION_DRY_RUN_PROPERTY_NAME,value = "false")
 
 //@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,value = "pretty, html:target/cucumber-report/HtmlReport1.html")
-@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty, json:target/json-reports/cucumberRapor1.json")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,value = "pretty, json:target/json-reports/cucumberRapor1.json")
 //@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,value = "pretty, junit:target/xml-report/cucumber1.xml")
+
 
 
 public class ParalelRun1 {
@@ -36,5 +37,12 @@ public class ParalelRun1 {
         2- pom.xml'e terminalden calistirmayi saglayacak
            ve bize guzel HTML raporlar olusturacak
            bazi plugin'leri eklemeliyiz
+        3- ekledigimiz plugin'lerde
+           calisacak Runner adedi ve
+           calisacak Runner'larin bulundugu yer
+           ve isimlerindeki ortak bolumu guncellememiz gerekir
+        4- Terminal'den mvn clean verify yazarak
+           pluginler uzerinden istenen Runner Class'lar calistirilabilir
+
      */
 }
